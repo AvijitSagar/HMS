710a04fe5730c18ec6dbe22ea2cb45126591280f
avijit biswas sagar <saggarr.work@gmail.com>
2023-12-22 22:43:54 +0600

    datatable a prothom thekei problem chilo. shob js file pacchilo na. datable akhon thikthak kaj kore. shob js file import kora hoyeche master blade a

--- resources/views/backend/master.blade.php
+++ resources/views/backend/master.blade.php
@@ -1025,10 +1025,20 @@
     <!-- CHART-CIRCLE JS-->
     <script src="{{ asset('/') }}backend/assets/plugins/circle-progress/circle-progress.min.js"></script>
 
-    <!-- INTERNAL DATA-TABLES JS-->
+    <!-- DATA-TABLES JS-->
     <script src="{{ asset('/') }}backend/assets/plugins/datatable/js/jquery.dataTables.min.js"></script>
     <script src="{{ asset('/') }}backend/assets/plugins/datatable/js/dataTables.bootstrap5.js"></script>
+    <script src="{{ asset('/') }}backend/assets/plugins/datatable/js/dataTables.buttons.min.js"></script>
+    <script src="{{ asset('/') }}backend/assets/plugins/datatable/js/buttons.bootstrap5.min.js"></script>
+    <script src="{{ asset('/') }}backend/assets/plugins/datatable/js/jszip.min.js"></script>
+    <script src="{{ asset('/') }}backend/assets/plugins/datatable/pdfmake/pdfmake.min.js"></script>
+    <script src="{{ asset('/') }}backend/assets/plugins/datatable/pdfmake/vfs_fonts.js"></script>
+    <script src="{{ asset('/') }}backend/assets/plugins/datatable/js/buttons.html5.min.js"></script>
+    <script src="{{ asset('/') }}backend/assets/plugins/datatable/js/buttons.print.min.js"></script>
+    <script src="{{ asset('/') }}backend/assets/plugins/datatable/js/buttons.colVis.min.js"></script>
     <script src="{{ asset('/') }}backend/assets/plugins/datatable/dataTables.responsive.min.js"></script>
+    <script src="{{ asset('/') }}backend/assets/plugins/datatable/responsive.bootstrap5.min.js"></script>
+    <script src="{{ asset('/') }}backend/assets/js/table-data.js"></script>
 
     <!-- INDEX JS -->
     <script src="{{ asset('/') }}backend/assets/js/index1.js"></script>
