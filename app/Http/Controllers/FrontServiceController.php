b0485691d09551273e8eebfcca4ba46d4c9149cb
avijit biswas sagar <saggarr.work@gmail.com>
2024-01-07 00:37:16 +0600

    frontend er service section a data show kora hoyeche

--- app/Http/Controllers/FrontServiceController.php
+++ app/Http/Controllers/FrontServiceController.php
@@ -2,11 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Member;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class FrontServiceController extends Controller
 {
     public function index(){
-        return view('frontend.service.index');
+        $member = Member::where('member_email', Auth::user()->email)->first();
+        return view('frontend.service.index', compact('member'));
     }
 }
--- resources/views/frontend/service/index.blade.php
+++ resources/views/frontend/service/index.blade.php
@@ -13,26 +13,29 @@
                     <th scope="col">Month</th>
                     <th scope="col">Service</th>
                     <th scope="col">Other</th>
-                    <th scope="col">Total</th>
+                    <th scope="col">Per Head</th>
                 </tr>
                 </thead>
                 <tbody>
-                <tr>
-                    <td>January 2024</td>
-                    <td>
-                        <ul>
-                            <li>Electricity:</li>
-                            <li>Water:</li>
-                            <li>Gas:</li>
-                            <li>Internet:</li>
-                            <li>Dish:</li>
-                            <hr>
-                            <li>Total:</li>
-                        </ul>
-                    </td>
-                    <td>40 &#2547;</td>
-                    <td>2200 &#2547;</td>
-                </tr>
+                    @foreach ($member->payments as $payment)
+                        <tr>
+                            <td>{{$payment->month_year}}</td>
+                            <td>
+                                <ul>
+                                    <li>Electricity: {{$payment->electric_bill}}</li>
+                                    <li>Water: {{$payment->water_bill}}</li>
+                                    <li>Gas: {{$payment->gas_bill}}</li>
+                                    <li>Internet: {{$payment->internet_bill}}</li>
+                                    <li>Dish: {{$payment->dish_bill}}</li>
+                                    <hr>
+                                    <li>Total: {{$payment->total_bill}} / {{$payment->total_members}}</li>
+                                </ul>
+                            </td>
+                            <td>{{$payment->other_expense}} &#2547;</td>
+                            <td>{{$payment->service_charge}} &#2547;</td>
+                        </tr>
+                    @endforeach
+                    
                 </tbody>
             </table>
         </div>
